bc7c1c464d0981ad9d92e43356bb639f7fc9a02a
Aaron Perrine <avsp.here@tamu.edu>
2017-03-20 23:50:33 +0000

    c4

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,34 +3,8 @@ require 'spec_helper.rb'
 describe SessionsController, :type => :controller do
     it "creates users" do
       fakeUser = User.create
-      post :create
-      
-      expect(flash[:notice]).to match('Welcome! Admin')
+      post :destroy
+      expect(response).to redirect_to(log_in_path)
+      #expect(response).to redirect_to('/root_path')
     end
-#   context 'when password is invalid' do
-#     it 'renders the page with error' do
-#       user = create(:user)
-
-#       post :create, session: { email: user.email, password: 'invalid' }
-
-#       expect(response).to render_template(:new)
-#       expect(flash[:notice]).to match('E-mail and/or password is incorrect.')
-#     end
-#   end
-  
-#   it "should pass params to subject" do
-#     post :create, :user => {:email => "abc@gmail.com"}
-#     assigns[:user].email.should == "abc@gmail.com"
-#   end
-
-#   context 'when password is valid' do
-#     it 'sets the user in the session and redirects them to their dashboard' do
-#       user = create(:user)
-
-#       post :create, session: { email: user.email, password: user.password }
-
-#       expect(response).to redirect_to '/dashboard'
-#       expect(controller.current_user).to eq user
-#     end
-#   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,16 +1,7 @@
 require 'spec_helper.rb'
 
 # describe UsersController do
-#     describe 'add user' do
-#         before :each do
-#             @fakeUser=double(User, :email => "hello@gmail.com")
-#             User.stub(:find_by_email).with('hello@gmail.com').and_return(@fakeUser)
-#         end 
+#     describe '#activate' do 
         
-#         it 'should call create' do
-#             expect(User.create).to eq(true)
-#             #@fakeUser.stub!(:create).and_return(true)
-#             #response.should redirect_to(final_path(@fakeUser))
-#         end 
 #     end 
 # end
