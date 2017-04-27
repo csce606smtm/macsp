82587109c77f4f68ba42b0437cfaaec0cee88050
Sungkeun Kim <danguria@gmail.com>
2017-04-27 01:55:12 +0000

    fixing db for heroku

--- app/models/division.rb
+++ app/models/division.rb
@@ -1,5 +1,5 @@
 class Division < ActiveRecord::Base
-    belongs_to :contests, class_name: 'Contest', foreign_key: 'user_id'
+    belongs_to :contests, class_name: 'Contest', foreign_key: 'contest_id'
     has_many :auctioneers
     has_many :qsheet
     #establish_connection :the_database
