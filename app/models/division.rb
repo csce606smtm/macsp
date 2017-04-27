18b76aa183856288e91e143ceb6e3382e88556b2
Sungkeun Kim <danguria@gmail.com>
2017-04-27 01:50:36 +0000

    fixing migration error

--- app/models/division.rb
+++ app/models/division.rb
@@ -1,5 +1,6 @@
 class Division < ActiveRecord::Base
-    belongs_to :contests
+    belongs_to :contests, class_name: 'Contest', foreign_key: 'user_id'
     has_many :auctioneers
     has_many :qsheet
+    #establish_connection :the_database
 end
